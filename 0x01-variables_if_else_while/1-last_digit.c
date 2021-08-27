3e122cd823ce1c064e180d3b7ec1000c8e9c07dd
omunga <cjomunga@gmail.com>
2021-08-27 07:22:22 -0700

    Update file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
 /**
  * main - prints the last digit of the random number
  * stored in the variable n
@@ -16,7 +16,7 @@ if (n % 10 > 5)
 printf("and is greater than 5\n");
 else if (n % 10 == 0)
 printf("and is 0\n");
-else if (n % 10 < 6 && n 10 != 0)
+else if (n % 10 < 6 && n % 10 != 0)
 printf("and is less than 6 and not 0\n");
 return (0);
 }
