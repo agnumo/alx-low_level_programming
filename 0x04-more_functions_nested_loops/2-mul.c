6b5774b49aa07f573eda47a8af3d9a51746abf78
omunga <cjomunga@gmail.com>
2021-09-01 23:26:42 -0700

    Add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: character to be multiplied by b
+ * @b: character to be multiplied by a
+ * Return: 0
+ */
+int mul(int a, int b);
+{
+	int a, b;
+	int c = a * b;
+
+	return ("%d", c);
+}
